cf45f37d83add5a73c932752f2d315861f673776
Ravi Anand <43475033+ravianandfbg@users.noreply.github.com>
2020-05-31 13:21:56 +0530

    Add files via upload

---
+++ ravi.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i,a[30];
+cout<<"Enter the number of array: "<<endl;
+cin>>n;
+cout<<"Enter the array: "<<endl;
+for(i=0;i<=n-1;i++)
+{
+cin>>a[i];
+}
+}
